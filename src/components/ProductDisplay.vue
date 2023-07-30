8a008f200dd8eb533a9194b13f525ddc18d56d4a
Kharisma Fajar <restuistyo@gmail.com>
2023-07-30 22:04:18 +0700

    Update skeleton and method
    Add skeleton and metos in product display

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,11 +1,75 @@
 <script>
 export default {
   name: "ProductDisplay",
+  data() {
+    return {
+      loading: false,
+      noIndex: 0,
+      rightCategory: false,
+      product: {},
+    };
+  },
+  methods: {
+    async panggilAPI() {
+      const response = await fetch(
+        `https://fakestoreapi.com/products/${this.noIndex}`
+      );
+      const result = await response.json;
+      return result;
+    },
+    async getProduct() {
+      this.loading = true;
+
+      if ((this.noIndex = 20)) {
+        this.noIndex = 1;
+      } else {
+        this.noIndex++;
+      }
+
+      let data = await this.panggilAPI();
+      if (
+        data.category == "men's clothing" ||
+        data.category == "women's clothing"
+      ) {
+        this.product = { data };
+        this.rightCategory = true;
+      } else {
+        this.rightCategory = false;
+      }
+
+      this.loading = false;
+    },
+    mounted() {
+      this.getProduct();
+    },
+  },
 };
 </script>
 
 <template>
   <div class="container">
+    <!-- Loading Skeleton -->
+    <div v-if="loading" class="product_container">
+      <!-- Left Side -->
+      <div class="skeleton_product_img"></div>
+
+      <!-- Right Side -->
+      <div>
+        <div class="skeleton_title_product"></div>
+        <div>
+          <div class="skeleton_category_product"></div>
+          <div class="skeleton_rating_product"></div>
+        </div>
+        <div class="skeleton_desc_product"></div>
+        <div class="skeleton_price_product"></div>
+        <div class="button_area">
+          <div class="skeleton_button"></div>
+          <div class="skeleton_button"></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Display Produk -->
     <div class="product_container">
       <div class="left_side">
         <img class="product_img" src="../assets/img/baju.png" alt="" />
