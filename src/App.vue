dc622faede32c19a9cfe3ab4aff4b813ad9f3e4f
Kharisma Fajar <restuistyo@gmail.com>
2023-07-30 07:31:04 +0700

    Push first
    add product display

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ProductDisplay/>
+</template>
+
+<script>
+import ProductDisplay from './components/ProductDisplay.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ProductDisplay
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+    name: 'ProductDisplay',
+}
+</script>
+
+<template>
+    <div>
+        <div class="product_container">
+            <div>
+                <img src="" alt="">
+            </div>
+            <div>
+                <h1 class="title_product">Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket</h1>\
+                <div>
+                    <span class="product_category">womens's clothing</span>
+                </div>
+                <p>100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON</p>
+                <h3 class="price_product">$30</h3>
+                <div class="button_area">
+                    <button class="btn btn_primary">Buy Now</button>
+                    <button class="btn btn_secondary">Next Product</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
